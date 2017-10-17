d368d98956953fb8c53ef6344e6793fd1088f10a
jbryan22 <jesse.bryan22@gmail.com>
2017-10-16 21:57:08 -0700

    Add project files.

---
+++ Gummi-Bear-Kingdom/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gummi_Bear_Kingdom.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
